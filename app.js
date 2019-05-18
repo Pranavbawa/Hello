033cc9f8456ac79a6c38af337cefb3d5676919b6
Pranav  bawa <pb389@njit.edu>
2019-05-18 17:34:14 -0400

    added hold and new button functionality

--- app.js
+++ app.js
@@ -1,16 +1,6 @@
 var scores, roundScore;
 
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
-
-//var dice = Math.floor(Math.random()*6)+1;
-document.querySelector('.dice').style.display = 'none';
-
-document.querySelector('#score-0').textContent = 0;
-document.querySelector('#score-1').textContent = 0;
-document.querySelector('#current-0').textContent = 0;
-document.querySelector('#current-1').textContent = 0;
+init();
 
 document.querySelector(".btn-roll").addEventListener('click',function(){
 
@@ -34,7 +24,7 @@ document.querySelector(".btn-roll").addEventListener('click',function(){
 });
 
 function nextPlayer(){
-			activePlayer === 0 ? activePlayer =1 : activePlayer =0;
+		activePlayer === 0 ? activePlayer =1 : activePlayer =0;
 		roundScore = 0;
 
 		document.querySelector('#current-0').textContent = 0;
@@ -46,5 +36,47 @@ function nextPlayer(){
 }
 
 
+document.querySelector('.btn-hold').addEventListener('click', function(){
+
+	scores[activePlayer]+= roundScore;
+	document.querySelector('#score-'+activePlayer).textContent = scores[activePlayer];
+	if(scores[activePlayer]>=20){
+		document.querySelector('#name-'+activePlayer).textContent = 'Winner!';
+		document.querySelector('.dice').style.display = 'none';
+		document.querySelector('.player-'+activePlayer+'-panel').classList.add('winner');
+		document.querySelector('.player-'+activePlayer+'-panel').classList.remove('active');
+	
+	}
+
+	else
+	{
+		nextPlayer();
+	}
+	
+});
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
+
+ function init(){
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 0;
+
+document.querySelector('.dice').style.display = 'none';
+
+document.querySelector('#score-0').textContent = 0;
+document.querySelector('#score-1').textContent = 0;
+document.querySelector('#current-0').textContent = 0;
+document.querySelector('#current-1').textContent = 0;
+document.querySelector('#name-0').textContent = 'Player 1';
+document.querySelector('#name-1').textContent = 'Player 2';
+document.querySelector('.player-0-panel').classList.remove('winner');
+document.querySelector('.player-1-panel').classList.remove('winner');
+document.querySelector('.player-0-panel').classList.remove('active');
+document.querySelector('.player-1-panel').classList.remove('active');
+document.querySelector('.player-0-panel').classList.add('active');
+ }
 
 
